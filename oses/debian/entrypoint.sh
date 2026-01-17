07fb407f58efcd3da901f50b0c04d5193069ac00
Lilija <lily@falixnodes.net>
2026-01-17 20:08:08 +0200

    fixy

--- oses/debian/entrypoint.sh
+++ oses/debian/entrypoint.sh
@@ -23,7 +23,7 @@
 
 # Default the TZ environment variable to UTC.
 #TZ=${TZ:-UTC}
-export TZ
+#export TZ
 
 # Set environment variable that holds the Internal Docker IP
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
