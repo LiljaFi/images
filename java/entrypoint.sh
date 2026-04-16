acceca6b4b2c89b79b25f341b283a30d8ec33486
Lilija <lily@falixnodes.net>
2026-04-16 15:41:12 +0300

    revert

--- java/entrypoint.sh
+++ java/entrypoint.sh
@@ -5,9 +5,5 @@
 # replacing the values.
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 
-if [ -z "${TIMEZONE}" ]; then
-  #TZ=${TIMEZONE}
-fi
-
 # shellcheck disable=SC2086
 exec env ${PARSED}
